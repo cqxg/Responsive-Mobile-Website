950865026e5e2414f1fc4efab0670e8e399935fc
cqxg <cqxg@yandex.ru>
2019-04-23 23:22:26 +0300

    feat:add filling

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+  
+    $('html, body').animate(
+      {
+        scrollTop: images
+      },
+      900
+    );
+  });
